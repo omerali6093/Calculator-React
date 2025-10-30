0835985991e3b09fb56b36b33f26ffa10882f71a
Omer Ali <omerali200765@gmail.com>
2025-10-31 01:23:27 +0500

    fully done

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,4 +1,4 @@
-import React, { use, useState }  from 'react';
+import React, { use, useState } from 'react';
 import Keys from './Keys'
 
 const Calculator = () => {
@@ -22,11 +22,47 @@ const Calculator = () => {
         "+",
         ".",
         "0",
-        "EQUAL"
+        "="
     ];
 
     const [showResult, setShowResult] = useState(false);
+    const [display, setDisplay] = useState('')
 
+
+    const maxLimit = 15;
+
+    function calculateResult () {
+        if(display.length !== 0) {
+            try {
+                let calcResult = eval(display);
+                calcResult = parseFloat(calcResult.toFixed(3));
+                setDisplay(calcResult);
+                setShowResult(true)
+            } catch (err) {
+                setDisplay("Error")
+            }
+        } else {
+            setDisplay("")
+        }
+    }
+
+    function handleButton(value) {
+        setShowResult(false)
+        if (value === 'AC') setDisplay("");
+        else if (value === "C") setDisplay(display.slice(0, -1));
+        else if (isOpeartor(value)) {
+            if (display == '' || isOpeartor(display[display.length - 1]))
+                return;
+            setDisplay(display + value)
+        }
+        else if(value === "=") calculateResult();
+        else setDisplay(display + value)
+    }
+
+
+    function isOpeartor(char) {
+        return ["*", "/", "%"].includes(char)
+    }
 
     const operationClass = "text-[1.2rem] tracking-[2px] flex gap-[5px] items-center text-[rgba(255,255,255,0.5)] justify-end"
     const resultClass = "text-[1.7rem] "
@@ -35,16 +71,22 @@ const Calculator = () => {
         <div className="min-w-[320px] bg-black flex-col gap-4 p-4 rounded-2xl">
             <div className="overflow-x-auto bg-[#141414] min-h-[100px]
              flex items-end justify-end flex-col p-4 rounded-[10px]">
-                <div className={`${showResult ? resultClass : operationClass }`}>Result</div>
-             </div>
+                <div className={`${showResult ? resultClass : operationClass}`}>
+                    {display}
+                </div>
+            </div>
 
             <div className="grid grid-cols-[repeat(4,1fr)] gap-[0.3rem] mt-5">
                 {keys.map((item, index) => (
-                    < Keys label={item} key={index} keyClass={item === 'EQUALS' &&
-                        'equals' }/>
+                    < Keys 
+                    label={item} 
+                    key={index} 
+                    keyClass={item === '=' && '='} 
+                    onButtonClick={handleButton}
+                    />
                 ))}
             </div>
-        </div>    
+        </div>
     )
 }
 
--- src/components/Keys.jsx
+++ src/components/Keys.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 
 
-const Keys = ({label, keyClass}) => {
+const Keys = ({label, keyClass, onButtonClick}) => {
     const equalClass = 'col-[span_2] bg-[#4ccdc6] text-[#1a261a] font-semibold hover:bg[4ccdc6]';
-    
+
     return (
         <div className={`bg-[#141414] flex cursor-pointer items-center
-        justify-center p-4 rounded-[5px] hover:bg-[#4ccdc742] ${keyClass && equalClass}`}>{label}</div>
+        justify-center p-4 rounded-[5px] hover:bg-[#4ccdc742] ${keyClass && equalClass}`}
+         onClick={()=>onButtonClick(label)}>
+            {label}
+            </div>
     )
 }
 
